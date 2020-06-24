d90caa687c995e11c470af870bd17cdcc526ef84
xuwenlong <330596595@qq.com>
2020-06-24 15:30:44 +0800

    新增地址列表；新增修改地址

--- pages/modify-address/modify-address.js
+++ pages/modify-address/modify-address.js
@@ -10,7 +10,7 @@ Page({
    * 页面的初始数据
    */
   data: {
-    title: '收货地址',
+    title: '选择收货地址',
     slideButtons: [
       {
         text: '设置为默认地址'
@@ -23,39 +23,32 @@ Page({
         text: '删除'
       }
     ],
-    addressList: [
-      {
-        id: 1,
-        contact: '赵四',
-        isDefault: 0,
-        mobilePhone: 13567890987,
-        contactAddress: '广东省 广州市 海珠区 新港中路397号'
-      },
-      {
-        id: 1,
-        contact: '张三',
-        isDefault: 1,
-        mobilePhone: 13567890987,
-        contactAddress: '广东省 广州市 海珠区 新港中路397号'
-      },
-      {
-        id: 1,
-        contact: '牛逼',
-        isDefault: 1,
-        mobilePhone: 13567890987,
-        contactAddress: '广东省 广州市 海珠区 新港中路397号'
-      }
-    ],
+    addressList: [],
     currentAddressId: '',
     // 联系人
     contact: '',
     contactAddress: '',
     mobilePhone: '',
-    isEdit: false
+    isEdit: false,
+    floors: [
+      ['30栋', '23栋'],
+      ['50L', '28L']
+    ],
+    floor: []
   },
   onLoad() {
-    // this.queryshopaddresslist()
+    this.queryshopaddresslist()
   },
+
+  // 选择联系人
+  phoneContact() {
+    wx.addPhoneContact({
+      success: res => {
+        console.log(res)
+      }
+    })
+  },
+
   // 获取收货地址列表
   queryshopaddresslist() {
     let params = {
@@ -82,17 +75,18 @@ Page({
     let self = this
     wx.chooseAddress({
     	success: res => {
-        self.setData({
-          contact: res.userName,
-          mobilePhone: res.telNumber,
-          contactAddress: res.provinceName + ' ' + res.cityName + ' ' + res.countyName + ' ' + res.detailInfo
-        })
+        console.log('address', res)
+        // self.setData({
+        //   contact: res.userName,
+        //   mobilePhone: res.telNumber,
+        //   contactAddress: res.provinceName + ' ' + res.cityName + ' ' + res.countyName + ' ' + res.detailInfo
+        // })
 
-        if (this.data.isEdit) {
-          self.updateshopaddress()
-        } else {
-          self.saveshopaddress()
-        }
+        // if (this.data.isEdit) {
+        //   self.updateshopaddress()
+        // } else {
+        //   self.saveshopaddress()
+        // }
     	},
     	fail: err => {
     		console.log('md:', err)
@@ -101,29 +95,26 @@ Page({
   },
   // 修改收货地址
   updateshopaddress() {
-    wx.navigateTo({
-      url: '../modify-address/modify-address'
-    })
-    // let self = this
-    // let params = {
-    //   id: this.data.currentAddressId,
-    //   contact: this.data.contact,
-    //   contactAddress: this.data.contactAddress,
-    //   isDefault: 0, // 1,非默认;0,默认
-    //   mobilePhone: this.data.mobilePhone,
-    //   openId: app.globalData.openId
-    // }
+    let self = this
+    let params = {
+      id: this.data.currentAddressId,
+      contact: this.data.contact,
+      contactAddress: this.data.contactAddress,
+      isDefault: 0, // 1,非默认;0,默认
+      mobilePhone: this.data.mobilePhone,
+      openId: app.globalData.openId
+    }
 
-    // request('user/updateshopaddress', params).then(res => {
-    //   if (res.data.code == 0) {
-    //     wx.showToast({
-    //       title: res.data.msg,
-    //       success() {
-    //         self.queryshopaddresslist()
-    //       }
-    //     })
-    //   }
-    // })
+    request('user/updateshopaddress', params).then(res => {
+      if (res.data.code == 0) {
+        wx.showToast({
+          title: res.data.msg,
+          success() {
+            self.queryshopaddresslist()
+          }
+        })
+      }
+    })
   },
   // 设置收货地址
   setAddress(e) {
@@ -218,7 +209,10 @@ Page({
       this.setdefaultshopaddress()
     } else if(btnIndex == 1) {
       // 编辑地址
-      this.updateshopaddress()
+      this.setData({
+        isEdit: true
+      })
+      this.chooseAddress()
 
     } else {
       // 删除
