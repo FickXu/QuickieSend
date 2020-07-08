aad75ca071a499a7e8e10abb42734a1abffb2422
xuwenlong <330596595@qq.com>
2020-07-08 11:30:41 +0800

    接口联调：实名认证

--- pages/setting/setting.js
+++ pages/setting/setting.js
@@ -1,3 +1,5 @@
+import request from '../api/request'
+
 // 获取app实例，包含了用户信息和全局方法
 const app = getApp();
 
@@ -7,49 +9,41 @@ Page({
   },
 	data: {
 		title: '个人资料',
-		cuCustomBGColor: '',
-		userInfo: {
-			avatarUrl: "https://wx.qlogo.cn/mmopen/vi_32/Q0j4TwGTfTINhLTx15w3Bm9iamcriaia0ELLTnyXtUJD9wHibQSOabeVSAqMmaDp8L1zTV1R2DlW9YnI5kOJ1fTlLg/132",
-			city: "Shenzhen",
-			country: "China",
-			gender: 1,
-			language: "zh_CN",
-			nickName: "Fick",
-			province: "Guangdong"
-		},
+		detials: {},
+		isRealName: 0,
+		isRealNameText: ''
 	},
-	// ready: function () {
-		// let self = this
-		// // console.log(app.globalData)
-		// let userInfo = {
-		// 	avatarUrl: "https://wx.qlogo.cn/mmopen/vi_32/Q0j4TwGTfTINhLTx15w3Bm9iamcriaia0ELLTnyXtUJD9wHibQSOabeVSAqMmaDp8L1zTV1R2DlW9YnI5kOJ1fTlLg/132",
-		// 	city: "Shenzhen",
-		// 	country: "China",
-		// 	gender: 1,
-		// 	language: "zh_CN",
-		// 	nickName: "Fick",
-		// 	province: "Guangdong"
-		// }
-		
-		// wx.getStorage({
-		// 	key: 'userInfo',
-		// 	success (res) {
-		// 		self.setData({
-		// 			userInfo: res.data
-		// 		})
-		// 	}
-		// })
-	// },
+
+	// 获取个人信息
+	onShow() {
+		request('user/info').then(res => {
+			this.setData({
+				detials: res.data.data,
+				isRealNameText: res.data.data.isRealName == 0 ? '未实名' : (res.data.data.isRealName == 1 ? '待审' : (res.data.data.isRealName == 2 ? '已认证' : '未通过')),
+				isRealName: res.data.data.isRealName
+			})
+			// console.log(this.data.detials)
+		})
+	},
+
 	// 个人信息
 	personInfoPage: function () {
 		wx.navigateTo({
 			url: '../person-info/person-info'
 		})
 	},
+	
 	// 地址列表
 	addressListPage: function () {
 		wx.navigateTo({
 			url: '../address-list/address-list'
+		})
+	},
+	
+	// 实名认证
+	userAuthPage: function () {
+		wx.navigateTo({
+			url: `../user-auth/user-auth?isRealName=${this.data.isRealName}`
 		})
 	},
 })
--- pages/user-auth/user-auth.js
+++ pages/user-auth/user-auth.js
@@ -1,3 +1,6 @@
+// const { default: request } = require("../api/request");
+import request from '../api/request'
+
 // 获取app实例，包含了用户信息和全局方法
 const app = getApp();
 
@@ -7,30 +10,103 @@ Page({
   },
 	data: {
 		title: '实名认证',
-		cuCustomBGColor: '',
-		birthday: '1992-02-3',
-		userInfo: {
-			avatarUrl: "https://wx.qlogo.cn/mmopen/vi_32/Q0j4TwGTfTINhLTx15w3Bm9iamcriaia0ELLTnyXtUJD9wHibQSOabeVSAqMmaDp8L1zTV1R2DlW9YnI5kOJ1fTlLg/132",
-			city: "Shenzhen",
-			country: "China",
-			gender: 1,
-			language: "zh_CN",
-			nickName: "Fick",
-			province: "Guangdong"
+		testImageUrl: "https://wx.qlogo.cn/mmopen/vi_32/Q0j4TwGTfTINhLTx15w3Bm9iamcriaia0ELLTnyXtUJD9wHibQSOabeVSAqMmaDp8L1zTV1R2DlW9YnI5kOJ1fTlLg/132",
+		params: {
+			// 身份证号码
+			idCard: '',
+			// 身份证正面
+			idCardFrong: 'https://wx.qlogo.cn/mmopen/vi_32/Q0j4TwGTfTINhLTx15w3Bm9iamcriaia0ELLTnyXtUJD9wHibQSOabeVSAqMmaDp8L1zTV1R2DlW9YnI5kOJ1fTlLg/132',
+			// 身份证反面
+			idCardReverse: 'https://wx.qlogo.cn/mmopen/vi_32/Q0j4TwGTfTINhLTx15w3Bm9iamcriaia0ELLTnyXtUJD9wHibQSOabeVSAqMmaDp8L1zTV1R2DlW9YnI5kOJ1fTlLg/132',
+			// 真实姓名
+			realName: '',
+			// 性别
+			sex: ''
 		},
-		genderArray: ['男', '女'],
-		genderIndex: 0
+		genderArray: ['', '男', '女'],
+		genderIndex: 0,
+		disabled: true,
+		details: {}
 	},
-	// 修改生日
-	DateChange(e) {
-		this.setData({
-			birthday: e.detail.value
+
+	onLoad(query) {
+		console.log(query)
+		request('user/info').then(res => {
+			this.setData({
+				details: res.data.data,
+				disabled: (query.isRealName == 0 || query.isRealName == 3) ? false : true,
+				genderIndex: res.data.data.sex
+			})
 		})
 	},
+
 	// 选择男女
 	PickerChange(e) {
 		this.setData({
-			genderIndex: e.detail.value
+			genderIndex: e.detail.value,
+			'params.sex': e.detail.value
+		})
+	},
+
+	// 输入事件
+	bindinput(e) {
+		console.log(e)
+		let key = e.currentTarget.dataset.key
+		let value = e.detail.value
+		let params = {
+			...this.data.params
+		}
+		params[key] = value
+		this.setData({
+			params: params
+		})
+	},
+
+	// 图片上传
+	uploaderImage(e) {
+
+		// 待审核和已通过不允许修改
+		if (disabled) return
+
+		let key = e.currentTarget.dataset.key
+		wx.chooseImage({
+			success(e) {
+				const tempFilePaths = e.tempFilePaths
+				console.log('=============', tempFilePaths)
+				wx.uploadFile({
+					url: 'http://fengchuhui.5gzvip.idcfengye.com/api/base/uploadone', //仅为示例，非真实的接口地址
+					filePath: tempFilePaths[0],
+					name: 'file',
+					// formData: {
+					// 	'file': tempFilePaths[0]
+					// },
+					success (res){
+						const data = res.data
+						console.log('upload file:', res)
+					},
+					fail (err) {
+						console.log('upload file fail', err)
+					}
+				})
+			}
+		})
+	},
+
+	// 提交实名信息
+	confirmUserAuth() {
+		let params = {
+			...this.data.params
+		}
+		request('user/realname', params).then(res => {
+			if (res.data.code === 10000) {
+				wx.showToast({
+					title: res.data.msg
+				})
+
+				setTimeout(() => {
+					wx.navigateBack()
+				}, 1500);
+			}
 		})
 	}
 })
