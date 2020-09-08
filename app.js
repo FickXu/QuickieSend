8c1f2d364b996d3f8953ec41e5b8c64f24449901
Fick <fickxu@163.com>
2020-09-08 14:19:47 +0800

    新增版本控制机制

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import request from './pages/api/request'
 App({
 	onLaunch: function() {
+    // 获取系统信息
     wx.getSystemInfo({
       success: e => {
         this.globalData.StatusBar = e.statusBarHeight;
@@ -17,6 +18,31 @@ App({
           this.globalData.isIpx = true
         }
       }
+    })
+    // 检测新版本
+    const updateManager = wx.getUpdateManager()
+    updateManager.onCheckForUpdate(function (res) {
+      // 请求完新版本信息的回调
+      console.log(res.hasUpdate)
+    })
+    updateManager.onUpdateReady(function () {
+      wx.showModal({
+        title: '更新提示',
+        content: '新版本已经准备好，是否重启应用？',
+        success: function (res) {
+          if (res.confirm) {
+            // 新的版本已经下载好，调用 applyUpdate 应用新版本并重启
+            updateManager.applyUpdate()
+          }
+        }
+      })
+    })
+    updateManager.onUpdateFailed(function () {
+      // 新版本下载失败
+      wx.showToast({
+        title: '下载失败',
+        icon: 'none'
+      })
     })
   },
   // 是否已经登录
